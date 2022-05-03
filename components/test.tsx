eff006f4fe3dd3f8552c7f9a504a03c28f450695
Aron Vaupel <aronvaupel@Arons-MacBook-Pro.local>
2022-05-03 10:11:59 +0200

    test

---
+++ components/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Test = () => {
+  //fetch something from shopify
+
+  return (
+    <>
+      <button className="bg-green-500">test</button>
+      <pre>{result}</pre>
+    </>
+  );
+};
+export default Test;
